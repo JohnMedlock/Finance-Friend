4fc9d7f5f68a9144a494d7066eb8da4ca2cd9730
James (Grant) Moore <107823094+jgrantmoore@users.noreply.github.com>
2025-02-07 22:15:10 -0500

    Updated header navlinks

--- frontend/src/pages/ProfilePage.jsx
+++ frontend/src/pages/ProfilePage.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import Header from '../components/Header';
 import './ProfilePage.css';
+import beautiful from '../assets/beautiful.jpg';
 
 const ProfilePage = () => {
   return (
     <div>
         <Header />
         <div className="profile-container">
-            <img className="pfp" src="https://yt3.googleusercontent.com/lN_qKLHlrrbGS_uEj-62MWeSsx7W2biOFfk2Dqz-mX1ConUV3Rop3fNeL24uZXx-77amh_c-Ng=s900-c-k-c0x00ffffff-no-rj" alt="Profile" />
+            <img className="pfp" src={beautiful} alt="Profile" />
             <hr className="profile-vert-hr"></hr>
             <div className="profile-info-section">
                 <h1>Profile</h1>
