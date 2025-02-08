8e172da3a11907daf0f755760035b7081a49fe27
James (Grant) Moore <107823094+jgrantmoore@users.noreply.github.com>
2025-02-07 23:45:12 -0500

    Updated Profile Page Layout
    Made the profile page resemble devpost more

--- frontend/src/pages/ProfilePage.jsx
+++ frontend/src/pages/ProfilePage.jsx
@@ -15,16 +15,20 @@ const result = array.map((item, index) => (
 
 const ProfilePage = () => {
   return (
-    <div>
+    <div className="profile-page">
         <Header />
+        <div className="banner-background">
+        </div>
+        
         <div className="profile-container">
             <img className="pfp" src={beautiful} alt="Profile" />
-            <hr className="profile-vert-hr"></hr>
             <div className="profile-info-section">
-                <h1>Profile</h1>
-                <p>Username: <strong>admin</strong></p>
-                <p>Location: <strong>Athens, GA</strong></p>
-                <p>Characters: {result}</p>
+                <h1>My Profile</h1>
+                <div className="profile-information">
+                    <p>Username: <strong>admin</strong></p>
+                    <p>Location: <strong>Athens, GA</strong></p>
+                    <p>Characters: {result}</p>
+                </div>
             </div>
         </div>
     </div>
