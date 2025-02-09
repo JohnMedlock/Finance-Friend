c64b47f7acb02cf05e899f51d9b2427dd0613d65
Rishab Seshadri <rishab.seshadri@gmail.com>
2025-02-09 02:25:59 -0500

    fixed db dupe error

--- backend/src/schemas/Model.js
+++ backend/src/schemas/Model.js
@@ -5,6 +5,8 @@ const modelSchema = new mongoose.Schema({
     modelName: { type: String, required: true },
     link: { type: String, required: true }
   }); // modelSchema
+
+  modelSchema.index({ userId: 1, modelName: 1 }, { unique: true });
   
   const Model = mongoose.model('Model', modelSchema);
   export default Model;
