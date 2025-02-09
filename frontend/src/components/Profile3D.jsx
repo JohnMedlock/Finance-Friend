e7623752ba015fbc6ec73b053c5c779be6d488fa
James (Grant) Moore <107823094+jgrantmoore@users.noreply.github.com>
2025-02-08 19:53:17 -0500

    The Monkey Update

--- frontend/src/components/Profile3D.jsx
+++ frontend/src/components/Profile3D.jsx
@@ -1,21 +1,31 @@
-import { Canvas } from '@react-three/fiber'
 import React from 'react'
+import { Canvas } from '@react-three/fiber'
+import { useGLTF, Environment } from '@react-three/drei'
 
-function Box() {
+function Model(props) {
+  const { nodes, materials } = useGLTF('/suzanne.gltf')
   return (
-    <mesh rotation={[0, 0, 0]}>
-      <boxGeometry args={[2, 2, 2]} />
-      <meshStandardMaterial color="purple" />
-    </mesh>
+    <group {...props} dispose={null}>
+      <mesh
+        castShadow
+        receiveShadow
+        geometry={nodes.Suzanne.geometry}
+        material={nodes.Suzanne.material}
+        position={[0, 1, 2]}
+      />
+    </group>
   )
 }
 
+useGLTF.preload('/suzanne.gltf')
+
 export function Profile3D() {
   return (
-    <Canvas style={{ height: '200px', width: '200px', borderRadius: '50%' }}>
+    <Canvas shadows camera={{ position: [0, 2, 5], fov: 60 }} >
       <ambientLight intensity={0.5} />
-      <pointLight position={[10, 10, 10]} />
-      <Box />
+      <directionalLight position={[5, 5, 5]} intensity={1.5} />
+      <Environment preset="sunset"/>
+      <Model/>
     </Canvas>
   )
 }
